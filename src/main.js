779edd63992d3bf31d473a697fcec6fb740d7321
Mathias <mathias.de.weerdt@gmail.com>
2019-10-03 15:45:46 +0200

    config update

--- src/main.js
+++ src/main.js
@@ -3,18 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Config from '../public/config'
 
 import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'http://localhost:3000/rooms-ws',
+  connection: Config.apiUrl + '/rooms-ws',
   vuex: {
     store,
     actionPrefix: 'SOCKET_',
     mutationPrefix: 'SOCKET_'
   }
-  // options: { path: "rooms-ws" } //Optional options
 }))
 
 Vue.config.productionTip = false
